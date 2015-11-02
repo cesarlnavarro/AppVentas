7950386df2a63a07662b3c62bbfec98bdfafa240
Cesar <cesarlnavarro@gmail.com>
2015-11-02 15:38:16 -0500

    Inicio proyecto

--- app/src/main/java/com/example/cesar/appventas/MainActivity.java
+++ app/src/main/java/com/example/cesar/appventas/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//prueba para subir datos al git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
